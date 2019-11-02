c3d2b513f9281dc2adeb28c41a66d9c5e951bcd8
moka2 <franco.mo@mail.utoronto.ca>
2019-11-02 15:17:23 -0400

    adding router to app.js

--- src/index.js
+++ src/index.js
@@ -1,35 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
-import App from './App';
-import About from './About'
-import Recommended from './Recommended'
-import Login from './Login'
-
-const routing = (
-    <Router>
-        <div>
-            <Route path="/" component={App} />
-            <Route path="/Recommended" component={Recommended} />
-            <Route path="/Login" component={Login} />
-            <Route path="/About" component={About} />
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/Recommended">Recommended</Link>
-                </li>
-                <li>
-                    <Link to="/Login">Login</Link>
-                </li>
-                <li>
-                    <Link to="/About">About</Link>
-                </li>
-            </ul>
-        </div>
-    </Router>
-)
+import routing from './App'
 
 
 ReactDOM.render(routing, document.getElementById('root'));
