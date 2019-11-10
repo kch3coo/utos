58c4924aeb28f5cd60536558f0f28e6d97fcd794
kch3coo <xia0weimo1999@gmail.com>
2019-11-09 19:36:45 -0500

    adding sideBar and modifying Navbar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+export default class Sidebar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          sidebarOpen: true
+        };
+        this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
+      }
+    
+      onSetSidebarOpen(open) {
+        this.setState({ sidebarOpen: open });
+      }
+  render() {
+    return (
+      <Sidebar
+        sidebar={<b>Sidebar content</b>}
+        open={this.state.sidebarOpen}
+        onSetOpen={this.onSetSidebarOpen}
+        styles={{ sidebar: { background: "white" } }}
+      >
+        <button onClick={() => this.onSetSidebarOpen(true)}>
+          Open sidebar
+        </button>
+      </Sidebar>
+    );
+  }
+}
